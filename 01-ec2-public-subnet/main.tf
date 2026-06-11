912db85e993d518c460e1e49701c9a570970d333
Suleiman <bmdanjumah@gmail.com>
2026-06-11 14:40:59 +0000

    provisioned on aws and added screenshots

--- 01-ec2-public-subnet/main.tf
+++ 01-ec2-public-subnet/main.tf
@@ -8,19 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    ec2 = "http://localhost:4566"
-    s3 = "http://localhost:4566"
-    lambda = "http://localhost:4566"
-    iam = "http://localhost:4566"
-  }
+  region                      = "eu-north-1"
 }
 
 // Creating VPC, Name, CIDR block, and Tags
@@ -40,7 +28,7 @@ resource "aws_subnet" "public-subnet-1" {
     vpc_id = aws_vpc.dev.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "us-east-1a"
+    availability_zone = "eu-north-1a"
 
     tags = {
         Name = "public_subnet"
@@ -51,7 +39,7 @@ resource "aws_subnet" "public-subnet-2" {
     vpc_id = aws_vpc.dev.id
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "us-east-1b"
+    availability_zone = "eu-north-1b"
 
     tags = {
         Name = "public_subnet"
@@ -87,10 +75,9 @@ resource "aws_route_table_association" "dev-public-2"{
 }
 
 resource "aws_instance" "public-instance-1"{
-    ami = "ami-5f2e2535"
-    instance_type = "t2.micro"
+    ami = "ami-05ec2ffaee0a0e6d4"
+    instance_type = "t3.micro"
     subnet_id  = "${aws_subnet.public-subnet-1.id}"
-    key_name = "key11"
 
     tags = {
         Name = "public-instance-1"
@@ -98,10 +85,9 @@ resource "aws_instance" "public-instance-1"{
 }
 
 resource "aws_instance" "public-instance-2"{
-    ami = "ami-5f2e2535"
-    instance_type = "t2.micro"
+    ami = "ami-05ec2ffaee0a0e6d4"
+    instance_type = "t3.micro"
     subnet_id  = "${aws_subnet.public-subnet-2.id}"
-    key_name = "key11"
 
     tags = {
         Name = "public-instance-2"
